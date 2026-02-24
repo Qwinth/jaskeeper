f4cdbd4e451379eb1f5a0bd489631fbcc3834901
Qwinth <vitaliygr2301@gmail.com>
2026-02-24 01:09:48 +0100

    Add primitives check functions

--- include/jaskeeper/primitives.hpp
+++ include/jaskeeper/primitives.hpp
@@ -10,3 +10,9 @@ class JUndefined {};
 class JNull {};
 
 using JPrimitive = std::variant<JStringPrimitive, JNumberPrimitive, JBooleanPrimitive, JUndefined, JNull>;
+
+bool isString(JPrimitive& v);
+bool isNumber(JPrimitive& v);
+bool isBoolean(JPrimitive& v);
+bool isUndefined(JPrimitive& v);
+bool isNull(JPrimitive& v);
